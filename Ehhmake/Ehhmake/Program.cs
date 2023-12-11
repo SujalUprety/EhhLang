82d8c9c87947ff60e8dc8e42098fd8879b880163
sujal <sujalsusu123@gmail.com>
2023-12-11 17:58:02 +0545

    Starting

---
+++ Ehhmake/Ehhmake/Program.cs
@@ -0,0 +1,14 @@
+﻿using Antlr4;
+using Ehhmake.GrammarContent;
+
+namespace Ehhmake;
+
+public class Program {
+
+    public static void Main(string[] args) {
+        var input = args[0];
+        
+        
+    }
+    
+}
