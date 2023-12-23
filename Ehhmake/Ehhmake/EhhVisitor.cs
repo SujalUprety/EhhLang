5f81b4ed7ef7f85e7281e7e79c206f7aa3241ea1
sujal <sujalsusu123@gmail.com>
2023-12-24 01:22:34 +0545

    very first flexible ehhmage

--- Ehhmake/Ehhmake/EhhVisitor.cs
+++ Ehhmake/Ehhmake/EhhVisitor.cs
@@ -3,6 +3,8 @@ using Ehhmake.Content;
 namespace Ehhmake;
 
 public class EhhVisitor : EhhBaseVisitor<object?> {
+    
+    private readonly Ehhmage _ehhmage = new();
 
     public override object? VisitStart(EhhParser.StartContext context) {
         if (context.LB().GetText() != "{") {
@@ -15,6 +17,50 @@ public class EhhVisitor : EhhBaseVisitor<object?> {
             return base.VisitStart(context);
         }
 
+        foreach (var attribPairContext in context.attribPair()) {
+            var attribName = attribPairContext.ID().GetText();
+            var attribValue = attribPairContext.attribValue().GetText();
+
+            switch (attribName) {
+                case nameof(Ehhmage.Attribute.width): {
+                    if (!int.TryParse(attribValue, out var width)) {
+                        Console.WriteLine("Width attribute value is not a number");
+                        break;
+                    }
+                    _ehhmage.SetWidth(width);
+                    break;
+                }
+
+                case nameof(Ehhmage.Attribute.height): {
+                    if (!int.TryParse(attribValue, out var height)) {
+                        Console.WriteLine("Height attribute value is not a number");
+                        break;
+                    }
+                    _ehhmage.SetHeight(height);
+                    break;
+                }
+
+                case nameof(Ehhmage.Attribute.background): {
+                    var background = attribValue.Split(',').Select(int.Parse).ToArray();
+                    if (background.Length != 3) {
+                        Console.WriteLine("Background attribute value is not a 3-tuple");
+                        break;
+                    }
+                    _ehhmage.SetBackground(background);
+                    
+                    break;
+                }
+                
+                case nameof(Ehhmage.Attribute.output): {
+                    _ehhmage.SetOutputName(attribValue);
+                    break;
+                }
+                    
+            }
+        }
+        
+        _ehhmage.CreateImage();
+        
         return base.VisitStart(context);
     }
     
--- Ehhmake/Ehhmake/Ehhmage.cs
+++ Ehhmake/Ehhmake/Ehhmage.cs
@@ -1,11 +1,47 @@
+using OpenCvSharp;
+
 namespace Ehhmake;
 
 public class Ehhmage {
-    private int _height { get; set; } = 1080;
-    private int _width { get; set; }= 1920;
     
-    private int[] _background { get; set; } = {0, 0, 0};
+    public enum Attribute {
+        width,
+        height,
+        background,
+        output
+    }
+    
+    private int _height  = 1080;
+    private int _width  = 1920;
 
-    private string _outputName { get; set; }= "ehhmage.png";
+    private int[] _background  = { 0, 0, 0 };
 
+    private string _outputName  = "ehhmage.png";
+
+
+    #region Setters
+
+    public void SetHeight(int height) {
+        _height = height;
+    }
+    
+    public void SetWidth(int width) {
+        _width = width;
+    }
+    
+    public void SetBackground(int[] background) {
+        _background = background;
+    }
+    
+    public void SetOutputName(string outputName) {
+        _outputName = outputName;
+    }
+
+    #endregion
+
+    public void CreateImage() {
+        var ehhmage = new Mat(_height, _width, MatType.CV_8UC3, new Scalar(_background[2], _background[1], _background[0]));
+        Cv2.ImWrite(_outputName, ehhmage);
+    }
+    
 }
